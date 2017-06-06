ba9499e494107dd06b8c3a0a9981b832b111caf4
Cullen Taylor <cullentaylor@outlook.com>
2017-06-06 15:34:25 -0500

    Split off event handling into functions
    * Add openDataFile function for opening the csv writer stream.
    * Add handlePlayerHurt function for writing player_hurt events to csv.
    * Add handlePlayerDeath function for writing player_death events to
      csv.
    * Add handleRoundEnd function for writing round_end events to csv.
    
    This slows the processing time only slightly, while improving
    readability and providing a nice function template for adding new
    events.
    
    Signed-off-by: Cullen Taylor <cullentaylor@outlook.com>

--- demo_processing.js
+++ demo_processing.js
@@ -9,97 +9,6 @@ var nodePath = process.env.NODE_PATH;
 var demo = require(nodePath + "/demofile/demo");
 const emptyCell = "";
 
-function parseDemoFile(path) {
-  fs.readFile(path, function (err, buffer) {
-    assert.ifError(err);
-    var csgoDataFile = "csgo_data.csv";
-
-    if (!fs.existsSync(csgoDataFile)) {
-      var writer = csvWriter({headers: ["eventType", "map", "attacker",
-                                        "victim", "weapon", "attackerHealth",
-                                        "headshot", "remainingHealth",
-                                        "remainingArmor", "hitgroup",
-                                        "winningTeam", "winReason"]});
-      writer.pipe(fs.createWriteStream(csgoDataFile));
-    }
-    else {
-      var writer = csvWriter({sendHeaders: false});
-      writer.pipe(fs.createWriteStream(csgoDataFile, {flags: "a"}));
-    }
-
-    var demoFile = new demo.DemoFile();
-    var mapName;
-
-    demoFile.on("start", () => {
-      mapName = demoFile.header.mapName;
-    });
-
-    demoFile.on("end", () => {
-      writer.end();
-    });
-
-    demoFile.gameEvents.on("player_hurt", e => {
-      let victim = demoFile.entities.getByUserId(e.userid);
-      let attacker = demoFile.entities.getByUserId(e.attacker);
-
-      if (e.hitgroup == 1) {
-        var headshot = true
-      }
-      else {
-        var headshot = false
-      }
-
-      var hitgroup = determineHitGroup(e.hitgroup);
-      if (victim && attacker) {
-        writer.write({eventType: "player_hurt", map: mapName,
-                      attacker: attacker.name, victim: victim.name,
-                      weapon: e.weapon, attackerHealth: attacker.health,
-                      headshot: headshot, remainingHealth: victim.health,
-                      remainingArmor: victim.armor, hitgroup: hitgroup,
-                      winningTeam: emptyCell, winReason: emptyCell});
-      }
-    });
-
-    demoFile.gameEvents.on("player_death", e => {
-      let victim = demoFile.entities.getByUserId(e.userid);
-      let attacker = demoFile.entities.getByUserId(e.attacker);
-
-      if (e.headshot) {
-        var hitgroup = "Head"
-      }
-      else {
-        var hitgroup = "None"
-      }
-
-      if (victim && attacker) {
-        writer.write({eventType: "player_death", map: mapName,
-                      attacker: attacker.name, victim: victim.name,
-                      weapon: e.weapon, attackerHealth: attacker.health,
-                      headshot: e.headshot, remainingHealth: victim.health,
-                      remainingArmor: victim.armor, hitgroup: hitgroup,
-                      winningTeam: emptyCell, winReason: emptyCell});
-      }
-    });
-
-    demoFile.gameEvents.on("round_end", e => {
-      if (e.winner == 2) {
-        var winningTeam = "Terrorists"
-      }
-      else if (e.winner == 3) {
-        var winningTeam = "Counter-Terrorists"
-      }
-      writer.write({eventType: "round_end", map: mapName,
-                    attacker: emptyCell, victim: emptyCell,
-                    weapon: emptyCell, attackerHealth: emptyCell,
-                    headshot: emptyCell, remainingHealth: emptyCell,
-                    remainingArmor: emptyCell, hitgroup: emptyCell,
-                    winningTeam: winningTeam, winReason: e.message.substring(13)});
-    });
-
-    demoFile.parse(buffer);
-  });
-}
-
 function determineHitGroup(hitgroup){
   var hitgroups = {
     "1": "Head",
@@ -112,6 +21,113 @@ function determineHitGroup(hitgroup){
     "default": "None"
   };
   return hitgroups[hitgroup] || hitgroups["default"];
+}
+
+function openDataFile(){
+  var csgoDataFile = "csgo_data.csv";
+  if (!fs.existsSync(csgoDataFile)) {
+    var writer = csvWriter({headers: ["eventType", "map", "attacker",
+                                      "victim", "weapon", "attackerHealth",
+                                      "headshot", "remainingHealth",
+                                      "remainingArmor", "hitgroup",
+                                      "winningTeam", "winReason"]});
+    writer.pipe(fs.createWriteStream(csgoDataFile));
+  }
+  else {
+    var writer = csvWriter({sendHeaders: false});
+    writer.pipe(fs.createWriteStream(csgoDataFile, {flags: "a"}));
+  }
+  return writer;
+}
+
+function handlePlayerHurt(demoFile, e, mapName, writer){
+  let victim = demoFile.entities.getByUserId(e.userid);
+  let attacker = demoFile.entities.getByUserId(e.attacker);
+
+  if (e.hitgroup == 1) {
+    var headshot = true
+  }
+  else {
+    var headshot = false
+  }
+
+  var hitgroup = determineHitGroup(e.hitgroup);
+  if (victim && attacker) {
+    writer.write({eventType: "player_hurt", map: mapName,
+                  attacker: attacker.name, victim: victim.name,
+                  weapon: e.weapon, attackerHealth: attacker.health,
+                  headshot: headshot, remainingHealth: victim.health,
+                  remainingArmor: victim.armor, hitgroup: hitgroup,
+                  winningTeam: emptyCell, winReason: emptyCell});
+  }
+}
+
+function handlePlayerDeath(demoFile, e, mapName, writer){
+  let victim = demoFile.entities.getByUserId(e.userid);
+  let attacker = demoFile.entities.getByUserId(e.attacker);
+
+  if (e.headshot) {
+    var hitgroup = "Head"
+  }
+  else {
+    var hitgroup = "None"
+  }
+
+  if (victim && attacker) {
+    writer.write({eventType: "player_death", map: mapName,
+                  attacker: attacker.name, victim: victim.name,
+                  weapon: e.weapon, attackerHealth: attacker.health,
+                  headshot: e.headshot, remainingHealth: victim.health,
+                  remainingArmor: victim.armor, hitgroup: hitgroup,
+                  winningTeam: emptyCell, winReason: emptyCell});
+  }
+}
+
+function handleRoundEnd(e, mapName, writer){
+  if (e.winner == 2) {
+    var winningTeam = "Terrorists"
+  }
+  else if (e.winner == 3) {
+    var winningTeam = "Counter-Terrorists"
+  }
+  writer.write({eventType: "round_end", map: mapName,
+                attacker: emptyCell, victim: emptyCell,
+                weapon: emptyCell, attackerHealth: emptyCell,
+                headshot: emptyCell, remainingHealth: emptyCell,
+                remainingArmor: emptyCell, hitgroup: emptyCell,
+                winningTeam: winningTeam, winReason: e.message.substring(13)});
+}
+
+function parseDemoFile(path) {
+  fs.readFile(path, function (err, buffer) {
+    assert.ifError(err);
+
+    var writer = openDataFile();
+    var demoFile = new demo.DemoFile();
+    var mapName;
+
+    demoFile.on("start", () => {
+      mapName = demoFile.header.mapName;
+    });
+
+    demoFile.on("end", () => {
+      writer.end();
+    });
+
+    demoFile.gameEvents.on("player_hurt", e => {
+      handlePlayerHurt(demoFile, e, mapName, writer);
+    });
+
+    demoFile.gameEvents.on("player_death", e => {
+      handlePlayerDeath(demoFile, e, mapName, writer);
+    });
+
+    demoFile.gameEvents.on("round_end", e => {
+      handleRoundEnd(e, mapName, writer);
+    });
+
+    demoFile.parse(buffer);
+  });
 }
 
 parseDemoFile(process.argv[2]);
